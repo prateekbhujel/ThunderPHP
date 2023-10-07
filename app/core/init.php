93138fcf0498e01795d7742d92ebfbb5c232e399
Pratik Bhujel <prateekbhujelpb@gmail.com>
2023-10-07 21:35:39 +0545

    Update #2 -- Basic Folder Structure and getting things ready.

---
+++ app/core/init.php
@@ -0,0 +1,5 @@
+<?php
+
+require 'functions.php';
+require 'App.php';
+
--- index.php
+++ index.php
@@ -1,46 +1,22 @@
 <?php
 
-	require 'functions.php';
+session_start();
 
-	$hero_titles = ['Home','Contact'];
-	$hero_titles = do_filter('hero_titles',$hero_titles);
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title></title>
-</head>
-<body>
+$minPHPVersion = '8.0';
+if(phpversion() < $minPHPVersion)
+	die("You need a minimum of PHP version $minPHPVersion to run this app.");
 
-	<style type="text/css">
-		body{
-			font-family: tahoma;
-		}
-	</style>
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOTPATH', __DIR__.DS);
 
-	<main style="max-width: 1000px;margin: auto;padding: 4px;">
-		<h1>This is a plugin website</h1>
-		<div style="display: flex;">
-			<div style="flex: 4;background-color: #eee;">
-				<div style="padding: 10px;">
+require 'config.php';
+require 'app'. DS .'core'. DS .'init.php';
 
-					<?php foreach ($hero_titles as $title):?>
-						<a href="" style="color:red"><?=$title?></a> | 
-					<?php endforeach?>
-					
-				</div>
-				<img src="images/image (3).jpg" style="max-width:100%;padding: 10px;">
-			</div>
-			<div style="flex: 1;background-color: #ddd;">
-				<img src="images/image (1).jpg" style="max-width:100%">
-				<img src="images/image (2).jpg" style="max-width:100%">
-				<?php do_action('images_section') ?>
-				<img src="images/image (5).jpg" style="max-width:100%">
-				<img src="images/image (4).jpg" style="max-width:100%">
-			</div>
-		</div>
-	</main>
-</body>
-</html>
+DEBUG ? ini_set('display_errors', 1) : ini_set('display_errors', 0);
+
+$ACTIONS    = [];
+$FILTERS    = [];
+$APP['URL'] = split_url();
+
+$app = new \Core\App();
+$app->index();
