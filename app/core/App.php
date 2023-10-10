2e0b3707e65e83dda58d22a193f3d29fdac297f1
Pratik Bhujel <prateekbhujelpb@gmail.com>
2023-10-10 13:45:07 +0545

    Update #6 -- made an hook like structure for do and add action function.

--- app/core/App.php
+++ app/core/App.php
@@ -9,8 +9,26 @@ class App
 {
 	public function index()
 	{
-		echo "everything is working";
-		echo "<pre>";
-		print_r(URL());
+		
+		do_action('before_controller');
+		do_action('controller');
+		do_action('after_controller');
+
+		do_action('before_view');
+
+		$before_content = ob_get_contents();
+		do_action('view');
+		$after_content = ob_get_contents();
+
+		if(str_replace($before_content, "", $after_content) == "")
+		{
+			if(page() != '404') {
+				redirect('404')	;
+			}
+		}
+
+		do_action('after_view');
+
+
 	}
 }
