fae08847ea556b65ddb3899b7e55dc9cfd321a65
Pratik Bhujel <prateekbhujelpb@gmail.com>
2023-10-12 16:03:05 +0545

    --Update #14 added function for insert, updtae, and delete (for model class) and added other classes too

---
+++ app/models/Image.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Core;
+
+/**
+ * Image Class
+ *
+ * This class provides functions for manipulating, resizing, cropping, and performing various operations on images.
+ */
+class Image
+{
+    /**
+     * Constructor for the Image class.
+     */
+    public function __construct()
+    {
+        dd("This is an Image class.");
+    }
+}
---
+++ app/models/Request.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Core;
+
+/**
+ *  Request Class is whenever we submit the form and recieve information from the url this is where we will deal with that information 
+ */
+class Request
+{
+	
+	function __construct()
+	{
+		dd("This is an Request class.");
+	}
+}
