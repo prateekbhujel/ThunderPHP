50fec29b6d2db0cf4157ab535bd1e18719f555c9
Pratik Bhujel <prateekbhujelpb@gmail.com>
2023-10-12 08:12:49 +0545

    -- Update #12 -- 'Database Class added with comments and filter functions(do/add)filters.'

--- config.php
+++ config.php
@@ -1,5 +1,50 @@
 <?php
 
-define('ROOT', 'https://localhost/thunderphp');
-
 define('DEBUG', true);
+
+define('APP_NAME', 'ThunderPHP App');
+define('APP_DESCRIPTION', 'This is best website !');
+
+if($_SERVER['SERVER_NAME'] == 'localhost')
+{
+	/** Database Name **/
+	define('DB_NAME', 'thunderphp_db');
+	
+	/** Database Username **/
+	define('DB_USER', 'root');
+
+	/** Database Password **/
+	define('DB_PASSWORD', '');
+
+	/** Database Hostname **/
+	define('DB_HOST', 'localhost');
+
+	/** Database Driver **/
+	define('DB_DRIVER', 'mysql');
+
+	/* Setting up the ROOT value */
+	define('ROOT', 'http://localhost/thunderphp');
+
+} else 
+{
+	/** Database Name **/
+	define('DB_NAME', 'thunderphp_db');
+	
+	/** Database Username **/
+	define('DB_USER', 'root');
+
+	/** Database Password **/
+	define('DB_PASSWORD', '');
+
+	/** Database Hostname **/
+	define('DB_HOST', 'localhost');
+
+	/** Database Driver **/
+	define('DB_DRIVER', 'mysql');
+	
+	/* Setting up the ROOT value */
+	define('ROOT', 'https://www.yourwebsite.com');
+
+}
+
+
--- plugins/basic-auth/plugin.php
+++ plugins/basic-auth/plugin.php
@@ -6,6 +6,7 @@ add_action('view', function() {
 	dd(get_value());
 });
 
+
 add_action('controller', function() {
 	
 	$arr = ['name'=>'Pratik Bhujel', 'age'=>28];
