4787f8d7e7a8736d6fb15482c90fca03aa57ad5d
Pratik Bhujel <prateekbhujelpb@gmail.com>
2023-10-29 10:31:09 +0545

    Update #24 . -- 'Command Line plugin tools created .'

---
+++ app/thunder/samples/view-sample.php
@@ -0,0 +1,4 @@
+<div style="padding: 20px; max-width: 600px; margin: auto; background-color: #eee;">
+	<center><h3>This is a plugin.</h3></center>
+	<center>This plugin is been rendered from the file : <?=plugin_path('views/view.php')?></center>
+</div>
