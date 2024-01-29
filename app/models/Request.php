68100e448bfc9d34bd7c459dc25e4ad17311dd6d
Pratik Bhujel <prateekbhujelpb@gmail.com>
2024-01-29 18:01:57 +0545

    Rebranded Thunder to PluginPHP....

--- app/models/Request.php
+++ app/models/Request.php
@@ -5,189 +5,156 @@ namespace Core;
 defined('ROOT') or die("Direct script access denied");
 
 /**
- * Request Class Handles the request.
+ * Request class
  */
 class Request
 {
-    // Maximum size for file uploads (in megabytes)
-    public $upload_max_size    = 20;
+	public $upload_max_size 	= 20;
+	public $upload_folder 		= 'uploads';
+	public $upload_errors 		= [];
+	public $upload_error_code 	= 0;
+	public $upload_file_types 	= [
+		'image/jpeg',
+		'image/png',
+		'image/webp',
+		'image/gif',
+	];
 
-    // Folder where uploads will be stored
-    public $upload_folder      = 'uploads';
+	public function method():string
+	{
+		return $_SERVER['REQUEST_METHOD'];
+	}
 
-    // Array to store upload errors
-    public $upload_errors      = [];
+	public function posted():bool
+	{
+		return $_SERVER['REQUEST_METHOD'] == 'POST';
+	}
 
-    // Code to represent the last upload error
-    public $upload_error_code  = 0;
-    
-    /*
-    * Lists of Supported file Type Goes here :
-    */
-    public $upload_file_types  = [
-        'image/jpeg',
-        'image/png',
-        'image/webp',
-        'image/gif',
-    ];
+	public function post(string $key = ''):string|array
+	{
+		if(empty($key))
+			return $_POST;
 
-    /**
-     * Returns the request method (GET, POST, etc.)
-     */
-    public function method(): string
-    {
-        return $_SERVER['REQUEST_METHOD'];
-    }
+		if(!empty($_POST[$key]))
+			return $_POST[$key];
 
-    /**
-     * Checks if the request method is POST
-     */
-    public function posted(): bool
-    {
-        return $_SERVER['REQUEST_METHOD'] == 'POST';
-    }
+		return '';
+	}
 
-    /**
-     * Gets POST data. If $key is empty, returns all POST data.
-     */
-    public function post(string $key = ''): string|array
-    {
-        if (empty($key))
-            return $_POST;
+	public function input(string $key, string $default = ''):string
+	{
+		if(!empty($_POST[$key]))
+			return $_POST[$key];
 
-        if (!empty($_POST[$key]))
-            return $_POST[$key];
+		return $default;
+	}
 
-        return '';
-    }
+	public function get(string $key = ''):string
+	{
+		if(empty($key))
+			return $_GET;
 
-    /**
-     * Gets input data based on key with a default value if not set.
-     */
-    public function input(string $key, string $default = ''): string
-    {
-        if (!empty($_POST[$key]))
-            return $_POST[$key];
+		if(!empty($_GET[$key]))
+			return $_GET[$key];
 
-        return $default;
-    }
+		return '';
+	}
 
-    /**
-     * Gets GET data. If $key is empty, returns all GET data.
-     */
-    public function get(string $key = ''): string
-    {
-        if (empty($key))
-            return $_GET;
+	public function files(string $key = ''):string|array
+	{
+		if(empty($key))
+			return $_FILES;
 
-        if (!empty($_GET[$key]))
-            return $_GET[$key];
+		if(!empty($_FILES[$key]))
+			return $_FILES[$key];
 
-        return '';
-    }
+		return '';
+	}
 
-    /**
-     * Gets FILES data. If $key is empty, returns all FILES data.
-     */
-    public function files(string $key = ''): string|array
-    {
-        if (empty($key))
-            return $_FILES;
+	public function all(string $key = ''):string|array
+	{
+		if(empty($key))
+			return $_REQUEST;
 
-        if (!empty($_FILES[$key]))
-            return $_FILES[$key];
+		if(!empty($_REQUEST[$key]))
+			return $_REQUEST[$key];
 
-        return '';
-    }
+		return '';
+	}
 
-    /**
-     * Gets REQUEST data. If $key is empty, returns all REQUEST data.
-     */
-    public function all(string $key = ''): string|array
-    {
-        if (empty($key))
-            return $_REQUEST;
+	public function upload_files(string $key = ''):string|array
+	{
+		$this->upload_errors 		= [];
+		$this->upload_error_code 	= 0;
 
-        if (!empty($_REQUEST[$key]))
-            return $_REQUEST[$key];
+		$uploaded = empty($key) ? [] : '';
 
-        return '';
-    }
+		if(!empty($this->files()))
+		{
+			$get_one = false;
+			if(!empty($key))
+				$get_one = true;
 
-    /**
-     * Handles the upload of files.
-     */
-    public function upload_files(string $key = ''): string|array
-    {
-        $this->upload_errors      = [];
-        $this->upload_error_code  = 0;
+			if($get_one && empty($this->files()[$key]))
+			{
+				$this->upload_errors['name'] = 'File not found';
+				return '';
+			}
 
-        $uploaded = empty($key) ? [] : '';
+			$uploaded = [];
+			foreach ($this->files() as $key => $file_arr) {
+				
+				if($file_arr['error'] > 0)
+				{
+					$this->upload_error_code = $file_arr['error'];
+					$this->upload_errors[] = "An error occured with file: ". $file_arr['name'];
+					continue;
+				}
 
-        if (!empty($this->files()))
-        {
-            $get_one = false;
-            if (!empty($key))
-                $get_one = true;
+				if(!in_array($file_arr['type'], $this->upload_file_types))
+				{
+					$this->upload_errors[] = "Invalid file type: ". $file_arr['name'];
+					continue;
+				}
 
-            if ($get_one && empty($this->files()[$key]))
-            {
-                $this->upload_errors['name'] = 'File not found';
-                return '';
-            }
+				if($file_arr['size'] > ($this->upload_max_size * 1024 * 1024))
+				{
+					$this->upload_errors[] = "File too large: ". $file_arr['name'];
+					continue;
+				}
+				
+				$folder = trim($this->upload_folder,'/') . '/';
+				$destination = $folder . $file_arr['name'];
 
-            $uploaded = [];
-            foreach ($this->files() as $key => $file_arr)
-            {
-                if ($file_arr['error'] > 0)
-                {
-                    $this->upload_error_code = $file_arr['error'];
-                    $this->upload_errors[] = "An error occurred with file: ". $file_arr['name'];
-                    continue;
-                }
+				$num = 0;
+				while(file_exists($destination) && $num < 10)
+				{
+					$num++;
+					$ext = explode(".", $destination);
+					$ext = end($ext);
 
-                if (!in_array($file_arr['type'], $this->upload_file_types))
-                {
-                    $this->upload_errors[] = "Invalid file type: ". $file_arr['name'];
-                    continue;
-                }
+					$destination = preg_replace("/\.$ext$/", "_" . rand(0,99) . ".$ext", $destination);
+				}
 
-                if ($file_arr['size'] > ($this->upload_max_size * 1024 * 1024))
-                {
-                    $this->upload_errors[] = "File too large: ". $file_arr['name'];
-                    continue;
-                }
-                
-                $folder = trim($this->upload_folder,'/') . '/';
-                $destination = $folder . $file_arr['name'];
+				if(!is_dir($folder))
+					mkdir($folder,0777,true);
 
-                $num = 0;
-                while (file_exists($destination) && $num < 10)
-                {
-                    $num++;
-                    $ext = explode(".", $destination);
-                    $ext = end($ext);
+				move_uploaded_file($file_arr['tmp_name'], $destination);
+				$uploaded[] = $destination;
 
-                    $destination = preg_replace("/\.$ext$/", "_" . rand(0,99) . ".$ext", $destination);
-                }
+				if($get_one)
+					break;
+			}
 
-                if (!is_dir($folder))
-                    mkdir($folder,0777,true);
+			if($get_one)
+				return $uploaded[0] ?? '';
 
-                move_uploaded_file($file_arr['tmp_name'], $destination);
-                $uploaded[] = $destination;
+			return $uploaded;
+			
+		}
 
-                if ($get_one)
-                    break;
-            }
+		return $uploaded;
+	}
 
-            if ($get_one)
-                return $uploaded[0] ?? '';
-
-            return $uploaded;
-            
-        }
-
-        return $uploaded;
-    }
+	
 }
